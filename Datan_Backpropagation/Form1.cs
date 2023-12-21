dda0b8c269188168b5c67d515f5956f04f31dd74
tarshibel <taraysbel.datan@cit.edu>
2023-12-21 23:48:33 +0800

    Add project files.

---
+++ Datan_Backpropagation/Form1.cs
@@ -0,0 +1,20 @@
+namespace Datan_Backpropagation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
